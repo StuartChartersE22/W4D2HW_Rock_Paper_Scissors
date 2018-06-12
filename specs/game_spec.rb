e57ff1343d6ac6d66433107608e9616f43ba0add
Ben Robinson <mentionbenjamin@gmail.com>
2018-06-12 15:50:15 +0100

    successful game test for draw

--- specs/game_spec.rb
+++ specs/game_spec.rb
@@ -3,8 +3,16 @@ require_relative("../models/game.rb")
 
 class TestGame < MiniTest::Test
 
-  def test_game__rock_scissors
+  def test_game__player_1_wins
     assert_equal("player 1 wins", Game.compare("rock", "scissors"))
+  end
+
+  def test_game__player_2_wins
+    assert_equal("player 2 wins", Game.compare("paper", "scissors"))
+  end
+
+  def test_game__draw
+    assert_equal("draw", Game.compare("rock", "rock"))
   end
 
 
